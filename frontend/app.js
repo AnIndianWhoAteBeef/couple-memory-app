6f02dda1994b87eccea8efc96d04ba4137387a4e
AIWAB <163100994+AnIndianWhoAteBeef@users.noreply.github.com>
2026-02-25 14:28:45 +0000

    제대로 된 건지 모르겠음

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,11 @@
+const modal = document.getElementById("diaryModal");
+const modalDate = document.getElementById("modalDate");
+const diaryText = document.getElementById("diaryText");
+const saveBtn = document.getElementById("saveDiary");
+
+let selectedDate = null;
+let selectedEmoji = "";
+
 document.querySelectorAll("button").forEach(btn => {
 
 btn.addEventListener("click", ()=>{
@@ -5,5 +13,17 @@ btn.addEventListener("click", ()=>{
 alert(btn.innerText + " 준비중!");
 
 });
+
+});
+
+document
+.querySelectorAll(".emoji")
+.forEach(btn=>{
+
+  btn.addEventListener("click",()=>{
+
+    selectedEmoji = btn.textContent;
+
+  });
 
 });
